2c5c5a9182be4e875955653b0d56d6697d6a8bac
Chishin <pro@xxgzs.org>
2019-02-26 16:14:56 +0800

    增加文件操作类

---
+++ ClassFile.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewLocalWiFiKey
+{
+    public static class ClassFile
+    {
+        #region 写文件
+        /****************************************
+         * 函数名称：WriteFile
+         * 功能说明：当文件不存时，则创建文件，并覆盖文件
+         * 参    数：Path:文件路径,Strings:文本内容
+         * 调用示列：
+         *           string Path = Server.MapPath("Default2.aspx");       
+         *           string Strings = "这是我写的内容啊";
+         *           Common.Utility.FileOperate.WriteFile(Path,Strings);
+        *****************************************/
+        /// <summary>
+        /// 写文件
+        /// </summary>
+        /// <param name="Path">文件路径</param>
+        /// <param name="Strings">文件内容</param>
+        public static void WriteFile(string Path, string Strings)
+        {
+
+            if (!System.IO.File.Exists(Path))
+            {
+                System.IO.FileStream f = System.IO.File.Create(Path);
+                f.Close();
+                f.Dispose();
+            }
+            System.IO.StreamWriter f2 = new System.IO.StreamWriter(Path, false, System.Text.Encoding.UTF8);
+            f2.WriteLine(Strings);
+            f2.Close();
+            f2.Dispose();
+        }
+        #endregion
+
+        #region 读文件
+        /****************************************
+         * 函数名称：ReadFile
+         * 功能说明：读取文本内容
+         * 参    数：Path:文件路径
+         * 调用示列：
+         *           string Path = Server.MapPath("Default2.aspx");       
+         *           string s = Common.Utility.FileOperate.ReadFile(Path);
+        *****************************************/
+        /// <summary>
+        /// 读文件
+        /// </summary>
+        /// <param name="Path">文件路径</param>
+        /// <returns></returns>
+        public static string ReadFile(string Path, string Encode = "UTF8")
+        {
+            string s = "";
+            if (!System.IO.File.Exists(Path))
+                s = "不存在相应的目录";
+            else
+            {
+                StreamReader f2 = new StreamReader(Path, System.Text.Encoding.GetEncoding(Encode));
+                s = f2.ReadToEnd();
+                f2.Close();
+                f2.Dispose();
+            }
+
+            return s;
+        }
+        #endregion
+
+        #region 追加文件
+        /****************************************
+         * 函数名称：FileAdd
+         * 功能说明：追加文件内容
+         * 参    数：Path:文件路径,strings:内容
+         * 调用示列：
+         *           string Path = Server.MapPath("Default2.aspx");     
+         *           string Strings = "新追加内容";
+         *           Common.Utility.FileOperate.FileAdd(Path, Strings);
+        *****************************************/
+        /// <summary>
+        /// 追加文件
+        /// </summary>
+        /// <param name="Path">文件路径</param>
+        /// <param name="strings">内容</param>
+        public static void FileAdd(string Path, string strings)
+        {
+            StreamWriter sw = File.AppendText(Path);
+            sw.Write(strings);
+            sw.Flush();
+            sw.Close();
+            sw.Dispose();
+        }
+        #endregion
+
+        #region 拷贝文件
+        /****************************************
+         * 函数名称：FileCoppy
+         * 功能说明：拷贝文件
+         * 参    数：OrignFile:原始文件,NewFile:新文件路径
+         * 调用示列：
+         *           string OrignFile = Server.MapPath("Default2.aspx");     
+         *           string NewFile = Server.MapPath("Default3.aspx");
+         *           Common.Utility.FileOperate.FileCoppy(OrignFile, NewFile);
+        *****************************************/
+        /// <summary>
+        /// 拷贝文件
+        /// </summary>
+        /// <param name="OrignFile">原始文件</param>
+        /// <param name="NewFile">新文件路径</param>
+        public static void FileCoppy(string OrignFile, string NewFile)
+        {
+            File.Copy(OrignFile, NewFile, true);
+        }
+
+        #endregion
+
+        #region 删除文件
+        /****************************************
+         * 函数名称：FileDel
+         * 功能说明：删除文件
+         * 参    数：Path:文件路径
+         * 调用示列：
+         *           string Path = Server.MapPath("Default3.aspx");    
+         *           Common.Utility.FileOperate.FileDel(Path);
+        *****************************************/
+        /// <summary>
+        /// 删除文件
+        /// </summary>
+        /// <param name="Path">路径</param>
+        public static void FileDel(string Path)
+        {
+            File.Delete(Path);
+        }
+        #endregion
+
+        #region 移动文件
+        /****************************************
+         * 函数名称：FileMove
+         * 功能说明：移动文件
+         * 参    数：OrignFile:原始路径,NewFile:新文件路径
+         * 调用示列：
+         *            string OrignFile = Server.MapPath("../说明.txt");    
+         *            string NewFile = Server.MapPath("../../说明.txt");
+         *            Common.Utility.FileOperate.FileMove(OrignFile, NewFile);
+        *****************************************/
+        /// <summary>
+        /// 移动文件
+        /// </summary>
+        /// <param name="OrignFile">原始路径</param>
+        /// <param name="NewFile">新路径</param>
+        public static void FileMove(string OrignFile, string NewFile)
+        {
+            File.Move(OrignFile, NewFile);
+        }
+        #endregion
+    }
+}
