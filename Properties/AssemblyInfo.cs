b7c064c971d84ec6d40bea4dcaa51aa20d1f4e02
chishin <pro@xxgzs.org>
2021-03-09 19:58:46 +0800

    修复wifi名称中有空格时,不能正常获取的bug

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Chishin")]
 [assembly: AssemblyProduct("ViewLocalWiFiKey")]
-[assembly: AssemblyCopyright("Copyright © Chisnin 2019")]
+[assembly: AssemblyCopyright("Copyright © Chisnin 2021")]
 [assembly: AssemblyTrademark("Chishin")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.2.2.0")]
+[assembly: AssemblyFileVersion("0.2.2.0")]
